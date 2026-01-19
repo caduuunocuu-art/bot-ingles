1943445ca60077b1f4a23c408162d5cc3cb5326a
caduuunocuu-art <caduuunocuu@gmail.com>
2026-01-18 23:32:25 -0300

    Update telegram_vip_funnel_bot_final.py

--- telegram_vip_funnel_bot_final.py
+++ telegram_vip_funnel_bot_final.py
@@ -663,9 +663,11 @@ async def on_shutdown(_):
     logger.info("Bot desligado e agendador parado")
 
 if __name__ == '__main__':
-    executor.start_polling(
-        dp,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-    )
+executor.start_polling(
+    dp,
+    on_startup=on_startup,
+    on_shutdown=on_shutdown,
+    skip_updates=True,
+    allowed_updates=["message", "chat_member", "my_chat_member"]
+)
+
